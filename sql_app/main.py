113653575d7532603f361e6545516751c385381a
Viet-dang-dung-GIT <viet.info.43@gmail.com>
2024-03-22 11:15:19 +0700

    user_mail

--- sql_app/main.py
+++ sql_app/main.py
@@ -27,7 +27,7 @@ def get_db(request: Request):
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+    db_user = crud.get_user_by_email(db, user_email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
