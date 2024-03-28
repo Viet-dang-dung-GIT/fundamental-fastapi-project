eedb88a2e1a7d80298e1be95f0ca1c6977454f5b
Viet-dang-dung-GIT <viet.info.43@gmail.com>
2024-03-28 09:57:41 +0700

    auth

--- app/endpoints/routers/users.py
+++ app/endpoints/routers/users.py
@@ -1,36 +1,50 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from starlette.responses import Response
 
-from app.crud.database import SessionLocal
 from app.crud import crud_users as crud, schemas
-from sql_app.main import get_db
+from app.crud.database import get_db
+from app.service.oauth2 import get_current_user
 
 # instance
-router = APIRouter()
+router = APIRouter(
+    prefix="/users",
+    tags=["users"]
+)
 
 
-@router.get("/users/", tags=["users"])
-async def read_users(db: Session = Depends(SessionLocal)):
-    list_users = crud.get_users(db)
-    return list_users
+@router.get("/", response_model=list[schemas.UserBase])
+async def read_users(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
+    list_user = await crud.get_all_users(db)
+    return list_user
 
 
-@router.get("/users/{user_id}", tags=["users"])
-async def read_user_by_id(user_id: int, db: Session = Depends(SessionLocal)):
-    return crud.get_user(db, user_id)
+@router.get("/id/{user_id}")
+async def read_user_by_id(user_id: int, db: Session = Depends(get_db),
+                          current_user: schemas.User = Depends(get_current_user)):
+    return await crud.get_user(db, user_id)
 
 
-@router.get("/users/{user_name}", tags=["users"])
-async def read_user(user_name: str, db: Session = Depends(SessionLocal)):
-    only_user = crud.get_user_by_name(db, user_name)
+@router.get("/name/{user_name}")
+async def read_user(user_name: str, db: Session = Depends(get_db),
+                    current_user: schemas.User = Depends(get_current_user)):
+    only_user = await crud.get_user_by_name(db, user_name)
     return only_user
 
 
-@router.get("/users/{user_email}")
-async def read_user_by_email(user_email: str, db: Session = Depends(SessionLocal)):
-    return crud.get_user_by_email(db, user_email)
+@router.get("/email/{user_email}")
+async def read_user_by_email(user_email: str, db: Session = Depends(get_db),
+                             current_user: schemas.User = Depends(get_current_user)):
+    return await crud.get_user_by_email(db, user_email)
 
 
-@router.post("/users/", response_model=schemas.User)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    return crud.create_user(db, user)
+@router.post("/", response_model=schemas.User)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db),
+                      current_user: schemas.User = Depends(get_current_user)):
+    return await crud.create_user(db, user)
+
+
+@router.put("/", response_model=schemas.UserCreate)
+async def update_user(user_id: int, user: schemas.UserCreate, db: Session = Depends(get_db),
+                      current_user: schemas.User = Depends(get_current_user)):
+    return await crud.update_user(db, user_id, user)
